878f97709c647a57918aed27422d8852d32781b9
TreeHarb05 <naoya0806.tmt@gmail.com>
2023-03-17 00:23:50 +0900

    reverse to a previous version on which users can search only by double-click

--- dblclick.js
+++ dblclick.js
@@ -1,30 +1,12 @@
 $(document).ready(function () {
     var lookupUrl = 'https://dictionary.cambridge.org/search/english/?q=';
 
-    var longpressDelay = 800;
-    var pressTimer;
-    var isLongPress = false;
-
-    $('body').on('mousedown touchstart', function (e) {
-        isLongPress = false;
-        pressTimer = setTimeout(function () {
-            isLongPress = true;
-            redirectToDictionary();
-        }, longpressDelay);
-    }).on('mouseup touchend', function (e) {
-        clearTimeout(pressTimer);
-    });
-
     $('body').dblclick(function (e) {
-        redirectToDictionary();
-    });
-
-    function redirectToDictionary() {
         var selectedText = getSelectedText();
         if (selectedText) {
             window.location.href = lookupUrl + encodeURIComponent(selectedText);
         }
-    }
+    });
 });
 
 function getSelectedText() {
